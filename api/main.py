2ed8dd78c1c120a03877db477edf2146218aec1d
Evgeniy Pelikh <eugene.p9g@gmail.com>
2024-09-26 15:39:50 +0200

    complte get method

--- api/main.py
+++ api/main.py
@@ -11,17 +11,21 @@ articles = {
     },
     'article2': {
         'title': 'Hello World on Python 2',
-        'text': 'Lorem  Lorem Lorem'
+        'text': 'Lorem Lorem Lorem'
     }
 }
 
 
 class Article(Resource):
-    def get(self):
+    def get(self, article_id=None):
+        if article_id:
+            return articles[article_id]
+
         return articles
 
 
-api.add_resource(Article, '/')
+api.add_resource(Article, '/articles', endpoint='article_list')
+api.add_resource(Article, '/articles/<article_id>', endpoint='article_detail')
 
 
 if __name__ == '__main__':
